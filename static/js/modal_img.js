eac2ebe56084f89ae34a571a8d0646b7ae94a3ff
VladislavTuzov <74977030+VladislavTuzov@users.noreply.github.com>
2021-08-13 16:41:54 +0300

    Delete static directory

--- static/js/modal_img.js
+++
@@ -1,7 +0,0 @@
-$('.close-modal-img').click(function () {
-    $('.modal-img').css({'display': 'none'})
-})
-$('.card__main img').click(function () {
-    $('.modal-img img').attr('src', $(this).attr('src'))
- $('.modal-img').css({'display':'flex'})
-})
