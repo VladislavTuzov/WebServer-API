89c35744634a64bec8665ea5855c1a1dafdc7965
VladislavTuzov <74977030+VladislavTuzov@users.noreply.github.com>
2021-08-13 05:03:24 +0300

    Delete forms directory

--- forms/upload.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField, FileField
-from wtforms.validators import DataRequired
-
-
-class UploadAvatar(FlaskForm):
-    fileName = FileField()
-    submit = SubmitField('Загрузить')
