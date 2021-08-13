613bdd7c3128b83b8361657aed42d281920b2c60
VladislavTuzov <74977030+VladislavTuzov@users.noreply.github.com>
2021-08-13 16:41:19 +0300

    Delete data directory

--- data/__all_models.py
+++
@@ -1,9 +0,0 @@
-from . import users
-from . import orders
-from . import positions
-from . import coupons
-# from . import history
-from . import main_info_site
-from . import kategory
-from . import user_black_list_ip
-from . import mass_act
